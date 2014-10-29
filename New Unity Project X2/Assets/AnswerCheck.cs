d1977ee4795b34d3138517a5589c86473fd98013
oitsmalab <oitsmalab@gmail.com>
2014-10-29 17:56:15 +0900

    10/29 赤石、青木作業。２次元配列を使って、表の状態を保存し、それを解答の２次元配列と比較できるようにした。

---
+++ New Unity Project X2/Assets/AnswerCheck.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class AnswerCheck : MonoBehaviour {
+	public int [,] Answer = new int[3,3]{
+		{1,7,4},
+		{2,8,5},
+		{3,9,6}
+	};
+	StoreState storestate;
+	private GameObject wakuwaku;
+
+
+	// Use this for initialization
+	public void OnClick () {
+		wakuwaku = GameObject.Find("wakuwakusan");
+		storestate = wakuwaku.GetComponent<StoreState>();
+
+		Debug.Log ("Clicked");
+
+		int correct = 0;
+		for (int i=0; i<3; i++) {
+			for(int j=0; j<3; j++){
+			//	Debug.Log("Answer = "+Answer[i,j]);
+				Debug.Log (Answer[i,j]+" vs " + storestate.posMatrix[i,j]);
+				if(Answer[i,j] == storestate.posMatrix[i,j]){
+					correct++;
+				}
+			}
+		}
+		if (correct == 9) {
+						Debug.Log ("!!!!");
+				}
+		Debug.Log (correct);
+
+	}
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project X2/Assets/StoreState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreState : MonoBehaviour {
+
+	public int [,] posMatrix =new int[3,3];
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
