93be4a618cfdf194b51c5c328175bf764c0c569d
yanoty3d <vzb10326@me.com>
2014-11-06 22:50:19 +0900

    FindInParents関数を追加。Hint1Text.GetComponent<Text>を用いて文字が表示できるようにした。 Hint1Textプレハブの位置を修正して、ボタンの近くに表示できるようにした。

--- New Unity Project X2/Assets/Hint.cs
+++ New Unity Project X2/Assets/Hint.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Hint : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+public class Hint: MonoBehaviour {
 	public int HintID = 0;
 	public GameObject Text;
 	
@@ -12,13 +12,13 @@ public class Hint : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerE
 		if(HintID == 1){
 			Debug.Log("XXX");
 
-			GameObject Hint1Text = Instantiate (Text, new Vector3(1, -251, 0), transform.rotation) as GameObject;
+			GameObject Hint1Text = Instantiate (Text) as GameObject;
 
 			var canvas = FindInParents<Canvas>(gameObject);
 			Hint1Text.transform.SetParent (canvas.transform, false);
 			Hint1Text.transform.SetAsLastSibling();
 			Hint1Text.GetComponent<RectTransform> ().position = GetComponent<RectTransform> ().position;
-
+			Hint1Text.GetComponent<Text>().text="XXX";
 			/*
 			Instantiate (Hint1Text, new Vector3(1, -251, 0), transform.rotation) ;
 			var canvas = FindInParents<Canvas>(gameObject);
@@ -29,6 +29,25 @@ public class Hint : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerE
 		}
 	}
 	
+
+	static public T FindInParents<T>(GameObject go) where T : Component
+		//GameObjectの親であり、型がTであるものを探しにいく
+	{
+		if (go == null) return null;
+		var comp = go.GetComponent<T>();
+		
+		if (comp != null)
+			return comp;
+		
+		Transform t = go.transform.parent;
+		while (t != null && comp == null)
+		{
+			comp = t.gameObject.GetComponent<T>();
+			t = t.parent;
+		}
+		return comp;
+	}
+
 
 	// Use this for initialization
 	void Start () {
