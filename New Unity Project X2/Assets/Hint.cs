a8d70dc8b3d0df44056a549afd6cb0def3593c09
oitsmalab <oitsmalab@gmail.com>
2014-11-06 19:01:02 +0900

    11/6 ヒントを生成（未完）　 謎のvarエラー

--- New Unity Project X2/Assets/Hint.cs
+++ New Unity Project X2/Assets/Hint.cs
@@ -1,18 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Reflection;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Hint : MonoBehaviour {
+public class Hint : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
 	public int HintID = 0;
+	public GameObject Text;
 	
 	public void OnClick(){
 		if(HintID == 1){
 			Debug.Log("XXX");
-		//	GUI.Label (new Rect (-268, -100, 400, 100), "ABCABC!!");
+
+			GameObject Hint1Text = Instantiate (Text, new Vector3(1, -251, 0), transform.rotation) as GameObject;
+
+			var canvas = FindInParents<Canvas>(gameObject);
+			Hint1Text.transform.SetParent (canvas.transform, false);
+			Hint1Text.transform.SetAsLastSibling();
+			Hint1Text.GetComponent<RectTransform> ().position = GetComponent<RectTransform> ().position;
+
+			/*
+			Instantiate (Hint1Text, new Vector3(1, -251, 0), transform.rotation) ;
+			var canvas = FindInParents<Canvas>(gameObject);
+			Hint1Text.transform.SetParent (canvas.transform, false);*/
+
 		}else if(HintID == 2){
 			Debug.Log("ZZZ");
 		}
 	}
-
+	
 
 	// Use this for initialization
 	void Start () {
